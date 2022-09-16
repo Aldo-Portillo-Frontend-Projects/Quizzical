60bac8edd0cf4398843c23dde239882bfc3da1c0
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-15 20:53:51 -0500

    Create an API data response file to configure code

---
+++ src/Api.js
@@ -0,0 +1,41 @@
+export const data = {
+    "response_code": 0,
+    "results": [{
+        "category": "Science: Computers",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "On which day did the World Wide Web go online?",
+        "correct_answer": "December 20, 1990",
+        "incorrect_answers": ["December 17, 1996", "November 12, 1990", "November 24, 1995"]
+    }, {
+        "category": "Science: Computers",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "What is known as &quot;the brain&quot; of the Computer?",
+        "correct_answer": "Central Processing Unit",
+        "incorrect_answers": ["Motherboard", "Graphics Processing Unit", "Keyboard"]
+    }, {
+        "category": "Science: Computers",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "What does the term MIME stand for, in regards to computing?",
+        "correct_answer": "Multipurpose Internet Mail Extensions",
+        "incorrect_answers": ["Mail Internet Mail Exchange", "Multipurpose Interleave Mail Exchange", "Mail Interleave Method Exchange"]
+    }, {
+        "category": "Science: Computers",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "What does RAID stand for?",
+        "correct_answer": "Redundant Array of Independent Disks",
+        "incorrect_answers": ["Rapid Access for Indexed Devices", "Range of Applications with Identical Designs", "Randomized Abstract Identification Description"]
+    }, {
+        "category": "Science: Computers",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "What does the &#039;S&#039; in the RSA encryption algorithm stand for?",
+        "correct_answer": "Shamir",
+        "incorrect_answers": ["Secure", "Schottky", "Stable"]
+    }]
+}
+
+//https://opentdb.com/api.php?amount=5&category=18&difficulty=medium&type=multiple
