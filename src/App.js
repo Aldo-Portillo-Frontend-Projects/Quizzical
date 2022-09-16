53e8b56efd160b2a11d53ea5a49000c634759725
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-15 20:54:58 -0500

    Render each question

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Question from './Question';
 
 function App() {
+
   return (
     <div className="App">
-      
+      <Question questionNum="0" />
+      <Question questionNum="1" />
+      <Question questionNum="2" />
+      <Question questionNum="3" />
+      <Question questionNum="4" />
     </div>
   );
 }
