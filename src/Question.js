fcee77dd034f3bf227582a222d05d21afd341914
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-16 10:29:34 -0500

    Create a function that randomizes answers

--- src/Question.js
+++ src/Question.js
@@ -2,6 +2,30 @@ import React from 'react'
 import { data } from './Api'
 
 export default function Question (props) {
+
+    function collectAnswers(){
+        let answer = [];
+        answer.push(
+            {answer: data.results[0].correct_answer, correct: true,}
+        )
+        let incorrectAnswerArr = data.results[0].incorrect_answers;
+        for (let i = 0; i < incorrectAnswerArr.length; i++){
+            answer.push({answer: incorrectAnswerArr[i], correct: false,})
+        }
+        for (let i = answer.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = answer[i];
+            answer[i] = answer[j];
+            answer[j] = temp;
+        }
+        return answer;
+        
+    }
+    
+console.log(collectAnswers())
+    
+
+   
     return(
         <div className='question'>
             <h1>{data.results[props.questionNum].question}</h1>
