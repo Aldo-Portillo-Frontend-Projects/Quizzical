7fac41939e061438b08ab86e4a5ac7fb36410565
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-16 11:24:08 -0500

    Render answers for each question

--- src/Answer.js
+++ src/Answer.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 export default function Answer (props) {
+    function clickHandle (){
+        console.log(props.correct)
+        //props.held ? props.held = false : props.held = true;
+        console.log(props.held)
+    }
     return (
-        <button>{props.value}</button>
+        <button correct={props.correct} onClick={clickHandle} held={props.held}>{props.value}</button>
     )
 }
--- src/Question.js
+++ src/Question.js
@@ -7,9 +7,9 @@ export default function Question (props) {
     function collectAnswers(){
         let answer = [];
         answer.push(
-            {value: data.results[0].correct_answer, correct: true,}
+            {value: data.results[props.questionNum].correct_answer, correct: true, held: false}
         )
-        let incorrectAnswerArr = data.results[0].incorrect_answers;
+        let incorrectAnswerArr = data.results[props.questionNum].incorrect_answers;
         for (let i = 0; i < incorrectAnswerArr.length; i++){
             answer.push({value: incorrectAnswerArr[i], correct: false,})
         }
@@ -28,14 +28,14 @@ export default function Question (props) {
 console.log(answer)
     
     const answerRender = answer.map(option => {
-        return <Answer value={option.value}/>
+        return <Answer value={option.value} correct={option.correct} held={option.held}/>
     })
    
     return(
         <div className='question'>
             <h1>{data.results[props.questionNum].question}</h1>
             <div className='answer-choices'>
-                {answerRender}
+                {answerRender} 
             </div>
         </div>
         
