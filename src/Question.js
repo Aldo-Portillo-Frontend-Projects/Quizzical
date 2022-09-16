6f4a1a0b916920f45ccf0a31aa0941e4390653f5
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-15 21:03:04 -0500

    Render answers

--- src/Question.js
+++ src/Question.js
@@ -2,11 +2,15 @@ import React from 'react'
 import { data } from './Api'
 
 export default function Question (props) {
-    console.log()
     return(
         <div className='question'>
             <h1>{data.results[props.questionNum].question}</h1>
-            
+            <div className='answer-choices'>
+                <button>{data.results[props.questionNum].correct_answer}</button>
+                <button>{data.results[props.questionNum].incorrect_answers[0]}</button>
+                <button>{data.results[props.questionNum].incorrect_answers[1]}</button>
+                <button>{data.results[props.questionNum].incorrect_answers[2]}</button>
+            </div>
         </div>
         
     )
