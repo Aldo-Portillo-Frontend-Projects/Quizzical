d731ce6ed0df9d95033bb43c9c0922cea5b21e0b
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-18 16:40:44 -0500

    Finish rendering components and adding logic to rendering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import './App.css';
+import React from 'react';
 import { questions } from './Api'
 
 function App() {
 
-  console.log(questions)
+  const [currentQuestion, setCurrentQuestion] = React.useState(0)
+  const [showScore, setShowScore] = React.useState(false)
+  const [score, setScore] = React.useState(0)
+
+  const handleAnswerClick = (isCorrect) =>{
+    currentQuestion < 4 ? setCurrentQuestion(prevQuestion => prevQuestion + 1) : setShowScore(true);
+
+    if(isCorrect === true){
+      setScore(prevScore => {
+        return prevScore + 1
+      })
+    }
+  }
+
+
   return (
-    <>
-    <div className='question-section'>
-      <div className='question-count'>
-        <span>Question 0</span>/{/*questions.length*/}
-      </div>
-      <div className='question-text'>Question ?</div>
+    <div className='app'>
+      {showScore ? 
+      (<div className='score-section'>You scored {score} out of {questions.length}</div>) : 
+      (
+      <>
+        <div className='question-section'>
+          <div className='question-count'>
+            <span>Question {currentQuestion + 1}</span>/{questions.length}
+          </div>
+          <div className='question-text'>{questions[currentQuestion].questionText}</div>
+        </div>
+        <div className='answer-section'>
+          {questions[currentQuestion].answerOptions.map((answerOption) => {
+            return <button onClick={() => handleAnswerClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+          })}
+        </div>
+      </>
+      )}
     </div>
-    <div className='answer-section'>
-      <button>Answer 1</button>
-      <button>Answer 2</button>
-      <button>Answer 3</button>
-      <button>Answer 4</button>
-    </div>
-    </>
   );
 }
 
